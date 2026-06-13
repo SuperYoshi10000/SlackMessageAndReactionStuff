8cdb308b56c7bc32195eddaf95749aef97b05eb4
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2026-06-12 22:20:53 -0400

    add home

--- app/listeners/events/app-home-opened.ts
+++ app/listeners/events/app-home-opened.ts
@@ -4,6 +4,7 @@ const appHomeOpenedCallback = async ({
   client,
   event,
   logger,
+  payload
 }: AllMiddlewareArgs & SlackEventMiddlewareArgs<'app_home_opened'>) => {
   // Ignore the `app_home_opened` event for anything but the Home tab
   if (event.tab !== 'home') {
@@ -11,26 +12,30 @@ const appHomeOpenedCallback = async ({
   }
 
   try {
+    const authUrl = `${process.env.OAUTH_AUTHORIZE_URL}%2F${payload.user}`; // last param is redirect_uri so this will add it to that
     await client.views.publish({
       user_id: event.user,
       view: {
         type: 'home',
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: `*Welcome home, <@${event.user}> :house:*`,
+        blocks: [{
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Please authorize this app (if you haven't already) before using its commands."
             },
-          },
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: 'Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.',
-            },
-          },
-        ],
+            "accessory": {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Authorize",
+                    "emoji": true
+                },
+                "value": "authorize",
+                "url": authUrl,
+                "action_id": "authorize",
+                "style": "primary"
+            }
+        }],
       },
     });
   } catch (error) {
