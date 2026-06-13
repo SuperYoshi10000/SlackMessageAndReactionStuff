85923cb2661639d335587d7b5aa0405950e77207
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2026-06-13 16:53:01 -0400

    handle invalid auth error

--- app/listeners/commands/blockkit.ts
+++ app/listeners/commands/blockkit.ts
@@ -34,7 +34,7 @@ const blockkit = async ({ ack, logger, respond, payload, client, context }: AllM
     }
   } catch (error) {
     logger.error(error);
-    if (result?.error === 'token_expired') {
+    if (result?.error === 'token_expired' || result?.error === 'invalid_auth') {
       clearUserAuth(payload.user_id);
       requestOauthMessage(client, payload.channel_id, payload.user_id);
       return;
