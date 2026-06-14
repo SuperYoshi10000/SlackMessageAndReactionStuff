b51680266912a350f1b0f195edf73ae326c44731
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2026-06-14 16:55:12 -0400

    copy github readme to home page

--- app/listeners/events/app-home-opened.ts
+++ app/listeners/events/app-home-opened.ts
@@ -42,6 +42,23 @@ const appHomeOpenedCallback = async ({
                 "action_id": "authorize",
                 "style": "primary"
             }
+        }, {
+          "type": "markdown",
+          "text": `# Message and Reaction Stuff
+This is a slack bot that lets you send a message using block kit, send an ephemeral message, and react with anything, all as you.
+
+## Block Kit
+To send a message using block kit, use \`/blockkit [message]\`  
+To send it as an ephemeral message to a specific user, use \`/blockkit @user [message]\`  
+To send it in a thread, use \`/blockkit [thread_ts] [message]\`  
+
+## Ephemeral Message
+To send an ephemeral ("Only visible to you") message, use \`/ephemeral @user [message]\`  
+To send it in a thread, use \`/ephemeral [thread_ts] @user [message]\`  
+
+## Editing Message
+Any normal message can be edited with \`/[command] edit [timestamp] [message]\`  
+Ephemeral messages cannot be edited, and non-block kit messages cannot be replaced with block kit (but block kit messages can be replaced with other block kit).`
         }],
       },
     });
