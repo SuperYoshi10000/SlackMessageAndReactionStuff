0416c17e02f21a19ad0ed862a36991195a7f2b7b
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2026-06-14 17:35:18 -0400

    add better error logging to /addreaction

--- app/listeners/commands/addreaction.ts
+++ app/listeners/commands/addreaction.ts
@@ -6,16 +6,22 @@ const addreaction = async ({ ack, logger, respond, payload, client, context }: A
     
     const [timestamp, ...reactions] = payload.text.split(/\s+/);
     
-    await Promise.all(reactions.map(reaction => client.reactions.add({
-        channel: payload.channel_id,
-        timestamp,
-        name: reaction.replace(/^:|:$/g, ''),
-        token: context.oauthUserToken
-    }).catch(error => client.chat.postEphemeral({
+    const errors: { reaction: string; error: any }[] = [];
+    for (const reaction of reactions) {
+        const name = reaction.replace(/^:|:$/g, '');
+        const result = await client.reactions.add({
+            channel: payload.channel_id,
+            timestamp,
+            name,
+            token: context.oauthUserToken
+        });
+        if (result.error) errors.push({ reaction: name, error: result.error });
+    }
+    if (errors.length > 0) client.chat.postEphemeral({
         channel: payload.channel_id,
         user: payload.user_id,
-        text: `Failed to add reaction :${reaction}:\n${error}`
-    }))));
+        text: `Failed to add one or more reactions:\n${errors.map(e => `:${e.reaction}: - ${e.error.error}`).join('\n')}`
+    })
   } catch (error) {
     logger.error(error);
   }
